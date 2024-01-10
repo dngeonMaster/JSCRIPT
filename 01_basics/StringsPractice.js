8243a6a257543648b5f423e1e75d25f47578af0f
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-10 06:39:44 +0000

    Practice

---
+++ 01_basics/StringsPractice.js
@@ -0,0 +1,12 @@
+//Declaring a string
+//node 01_basics/StringsPractice.js
+const string1="Kaushik"
+const string2='Kaushik'
+const string3=`Kaushik`
+
+console.log(string1+string2+string3);
+
+//Ways to access a character in string
+
+console.log(string1.charAt(1));
+console.log(string1[1]);
