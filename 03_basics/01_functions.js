aa29443fd818635dbe261b2498389406b090072d
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-11 19:57:44 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+//node 03_basics/01_functions
+
+//Wrapping up of a peice of code in a fackage -> Function
+
+//Function Definition
+function sayMyName(){
+    console.log("I");
+    console.log("am");
+    console.log("Heisenberg!!");
+}
+
+//Function call
+//sayMyName //This is the reference of the function
+
+//sayMyName()  //This is function execution
+
+//add two numers
+
+function addTwoNum(number1,number2){  //Here we call these values as parameters
+    console.log(number1+number2)
+}
+addTwoNum(3,6)  //while calling a fucntion ,we call these values as argument
+
