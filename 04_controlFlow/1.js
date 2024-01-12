9191825c410b4c83adc4f27b326599ec0e3070f2
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-12 20:43:33 +0000

    if

--- 04_controlFlow/1.js
+++ 04_controlFlow/1.js
@@ -2,9 +2,21 @@
 
 //If - statement
 //03_basics/04_controlFlow
-const isUserLoggedin=true
-if(isUserLoggedin){
-    console.log("Logged in");
-}
+// const isUserLoggedin=true
+// if(isUserLoggedin){
+//     console.log("Logged in");
+// }
 //<,>,<=,>=,==,!=,===(checks the type as well)
+//also have the concept of scope
 
+const balance=600
+
+//if(balance >500) console.log("test"); ///implicite scope
+
+//Nesting
+if (balance<500) {
+    console.log("yes");}
+    else if(balance<750){
+        console.log("yes");
+    }
+    
