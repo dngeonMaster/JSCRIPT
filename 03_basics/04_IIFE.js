e69eada588608d8b04b05c5eb990e3023824e05c
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-12 19:53:03 +0000

    ..

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -4,9 +4,9 @@
 //node 03_basics/04_IIFE.js
 
 //IIFE ()()
-(function chai(){
+(function chai(){  //Named IIFE
     console.log("DB Connected");
 })();
-( ()=>{
+( ()=>{          //Unnamed IIFE
     console.log("DB2 connected because of ; ");
 })();
