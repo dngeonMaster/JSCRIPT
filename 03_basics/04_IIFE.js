d92ec39621fe4482d2228777c3b9e91013c04bcd
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-12 19:48:39 +0000

    iife

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,9 @@
+//Immediately invoked function Expression(IIFE)
+
+//Executing the function right after declaring it
+//node 03_basics/04_IIFE.js
+
+//IIFE ()()
+(function chai(){
+    console.log("DB Connected");
+})()
