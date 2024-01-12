52362956e8201dd2dd947e0356062b3a0c7fe74a
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-12 20:35:18 +0000

    ...

---
+++ 04_controlFlow/1.js
@@ -0,0 +1,10 @@
+//Control Flow
+
+//If - statement
+//03_basics/04_controlFlow
+const isUserLoggedin=true
+if(isUserLoggedin){
+    console.log("Logged in");
+}
+//<,>,<=,>=,==,!=,===(checks the type as well)
+
