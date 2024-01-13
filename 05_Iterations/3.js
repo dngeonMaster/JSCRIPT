26145583c1ece79f09a6449cf54a2f265dd4bc33
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-13 18:46:03 +0000

    ///

--- 05_Iterations/3.js
+++ 05_Iterations/3.js
@@ -11,9 +11,35 @@ for (const num of arr) {
 
 const greetings="Hello World!"
 for (const greet of greetings) {
-    console.log(`Each character is ${greet}`);
+    //console.log(`Each character is ${greet}`);
 }
 
 //MAPS
 
-const map=new Map()
+const map=new Map()  //A key in the map may only occur once
+map.set('In',"India")
+map.set('USA',"United States of America")
+map.set('UK',"United Kingdom")
+map.set('In',"India")  //Here india wont go in the map as it takes only unique value
+
+//console.log(map);
+
+//Looping in Map
+for (const [ key , value] of map) {  //Destructuring 
+    console.log(key ,"->",value);
+    
+    
+}
+
+//Looping in Object
+
+// const myObject ={
+//     'game1' : 'NFS',
+//     'game2' : 'GTA',
+// }
+
+// for (const key of myObject) {  ///Oject is not iteratabble by this method
+//     console.log(key);
+    
+// }
+
