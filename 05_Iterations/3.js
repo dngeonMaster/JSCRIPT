208f50ededc56a35ea431be921d4caf64236d895
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-13 10:01:25 +0000

    for of

---
+++ 05_Iterations/3.js
@@ -0,0 +1,17 @@
+//For or ->Array Specific loops
+//node 05_Iterations/3.js
+// ["","",""]
+// [{},{},{}]  Objects inside array
+
+const arr=[1,2,3,4,5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings="Hello World!"
+for (const greet of greetings) {
+    console.log(`Each character is ${greet}`);
+}
+
+//MAPS
