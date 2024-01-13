8bb3a2f1cb410f8175bd69bc85fe9a224b8fb5ae
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-13 18:53:10 +0000

    //

---
+++ 05_Iterations/4.js
@@ -0,0 +1,22 @@
+//node 05_Iterations/4.js
+const myObject={
+    js : "JavaScript",
+    cpp : "C++",
+    rb : "Ruby",
+    swift : "Swift by apple",
+}
+
+//for in loops for objects
+
+for (const key in myObject) {
+    // console.log(myObject[key]);  //We will get the values
+    // console.log(key);    //We will get the keys
+}
+
+
+//For in -> Array
+const programming=["js","cpp","python","ruby"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
