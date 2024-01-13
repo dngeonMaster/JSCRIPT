c92fc3dfee6e9710423f93896ce34ba443036f21
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-13 20:13:46 +0000

    Map

---
+++ 05_Iterations/7.js
@@ -0,0 +1,16 @@
+//MAP Operation
+//node 05_Iterations/7.js
+const myNumbers=[1,2,3,4,5,6,7,8,,9,10]
+// const newNums=myNumbers.map((num) => num+10)
+
+
+
+
+//Chaiining
+
+const newNums=myNumbers
+                      .map((num)=>num+10) 
+                      .map((num) => num*2)
+                      .map((num) => num+1)
+
+console.log(newNums);
