6b3518ea6ed25bb005c0874ef5637d2db066e103
Kaushik Borah <117542258+dngeonMaster@users.noreply.github.com>
2024-01-23 20:02:27 +0000

    Promise

--- Promises/promises.js
+++ Promises/promises.js
@@ -11,10 +11,20 @@ const promise1=new Promise(function (resolve,reject){
     //like DataBase calls , cryptography , network
     setTimeout(function(){
         console.log("Async Task is Complete");
+        resolve()  //at this point promise will get connect by .then
     },3000)
 }) 
 
 //Consuming Promise
 promise1.then(function(){
-    console.log("Promise consumed");
+    console.log("Promise consumed");  //after resolve()
+})
+
+new Promise(function (resolve,reject){
+    setTimeout(function(){
+        console.log("HIIII");
+        resolve()
+    },2000)
+}).then(function(){
+    console.log("I am fine");
 })
